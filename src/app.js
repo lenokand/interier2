421f20686ee006903b03b14a248b1b5429092102
unknown <shtanko1991@gmail.com>
2021-12-08 14:40:39 +0300

    first commit

--- src/app.js
+++ src/app.js
@@ -1,17 +1,81 @@
-import 'bootstrap'
-import './js/router'
-import Storage from './js/_storage'
+// import 'fullpage.js'
 
-Storage.fetchCart()
+// new fullpage('#fullpage', {
+// 	//options here
+// 	autoScrolling:true,
+// 	scrollHorizontally: true,
 
-document.onclick = function(e) {
-    if(e.target.classList.contains('add-to-cart-button')) {
-        Storage.addToCart({
-            id: e.target.getAttribute('data-id'),
-            title: e.target.getAttribute('data-title'),
-            image: e.target.getAttribute('data-image'),
-            price: e.target.getAttribute('data-price'),
-        })
-        e.target.setAttribute('disabled', true)
-    }
-}
+
+//     //Scrolling
+// 	css3: true,
+// 	scrollingSpeed: 700,
+// 	autoScrolling: true,
+// 	fitToSection: true,
+// 	fitToSectionDelay: 1000,
+// 	scrollBar: false,
+// 	easing: 'easeInOutCubic',
+// 	easingcss3: 'ease',
+// 	loopBottom: false,
+// 	loopTop: false,
+// 	loopHorizontal: true,
+// 	continuousVertical: false,
+// 	continuousHorizontal: false,
+// 	scrollHorizontally: false,
+// 	interlockedSlides: false,
+// 	dragAndMove: false,
+// 	offsetSections: false,
+// 	resetSliders: false,
+// 	fadingEffect: false,
+// 	normalScrollElements: '#element1, .element2',
+// 	scrollOverflow: false,
+// 	scrollOverflowReset: false,
+// 	scrollOverflowOptions: null,
+// 	touchSensitivity: 15,
+// 	bigSectionsDestination: null,
+
+// 	//Accessibility
+// 	keyboardScrolling: true,
+// 	animateAnchor: true,
+// 	recordHistory: true,
+
+// 	//Design
+// 	controlArrows: true,
+// 	verticalCentered: true,
+// 	sectionsColor : ['#ccc', '#fff'],
+// 	paddingTop: '3em',
+// 	paddingBottom: '10px',
+// 	fixedElements: '#header, .footer',
+// 	responsiveWidth: 0,
+// 	responsiveHeight: 0,
+// 	responsiveSlides: false,
+// 	parallax: false,
+// 	parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+// 	dropEffect: false,
+// 	dropEffectOptions: { speed: 2300, color: '#F82F4D', zIndex: 9999},
+// 	waterEffect: false,
+// 	waterEffectOptions: { animateContent: true, animateOnMouseMove: true},
+// 	cards: false,
+// 	cardsOptions: {perspective: 100, fadeContent: true, fadeBackground: true},
+
+// 	//Custom selectors
+// 	sectionSelector: '.section',
+// 	slideSelector: '.slide',
+
+// 	lazyLoading: true,
+
+// 	//events
+// 	onLeave: function(origin, destination, direction){},
+// 	afterLoad: function(origin, destination, direction){},
+// 	afterRender: function(){},
+// 	afterResize: function(width, height){},
+// 	afterReBuild: function(){},
+// 	afterResponsive: function(isResponsive){},
+// 	afterSlideLoad: function(section, origin, destination, direction){},
+// 	onSlideLeave: function(section, origin, destination, direction){}
+
+// });
+
+// //methods
+// fullpage_api.setAllowScrolling(false);
+
+
