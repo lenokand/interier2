d7313d07e7358b0f78db384c9ecb6b5f01d90d8d
unknown <shtanko1991@gmail.com>
2021-12-20 17:35:13 +0300

    main

--- src/app.js
+++ src/app.js
@@ -1,82 +1,4 @@
-// import 'fullpage.js'
 
-// new fullpage('#fullpage', {
-// 	//options here
-// 	autoScrolling:true,
-// 	scrollHorizontally: true,
-
-
-//     //Scrolling
-// 	css3: true,
-// 	scrollingSpeed: 700,
-// 	autoScrolling: true,
-// 	fitToSection: true,
-// 	fitToSectionDelay: 1000,
-// 	scrollBar: false,
-// 	easing: 'easeInOutCubic',
-// 	easingcss3: 'ease',
-// 	loopBottom: false,
-// 	loopTop: false,
-// 	loopHorizontal: true,
-// 	continuousVertical: false,
-// 	continuousHorizontal: false,
-// 	scrollHorizontally: false,
-// 	interlockedSlides: false,
-// 	dragAndMove: false,
-// 	offsetSections: false,
-// 	resetSliders: false,
-// 	fadingEffect: false,
-// 	normalScrollElements: '#element1, .element2',
-// 	scrollOverflow: false,
-// 	scrollOverflowReset: false,
-// 	scrollOverflowOptions: null,
-// 	touchSensitivity: 15,
-// 	bigSectionsDestination: null,
-
-// 	//Accessibility
-// 	keyboardScrolling: true,
-// 	animateAnchor: true,
-// 	recordHistory: true,
-
-// 	//Design
-// 	controlArrows: true,
-// 	verticalCentered: true,
-// 	sectionsColor : ['#ccc', '#fff'],
-// 	paddingTop: '3em',
-// 	paddingBottom: '10px',
-// 	fixedElements: '#header, .footer',
-// 	responsiveWidth: 0,
-// 	responsiveHeight: 0,
-// 	responsiveSlides: false,
-// 	parallax: false,
-// 	parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
-// 	dropEffect: false,
-// 	dropEffectOptions: { speed: 2300, color: '#F82F4D', zIndex: 9999},
-// 	waterEffect: false,
-// 	waterEffectOptions: { animateContent: true, animateOnMouseMove: true},
-// 	cards: false,
-// 	cardsOptions: {perspective: 100, fadeContent: true, fadeBackground: true},
-
-// 	//Custom selectors
-// 	sectionSelector: '.section',
-// 	slideSelector: '.slide',
-
-// 	lazyLoading: true,
-
-// 	//events
-// 	onLeave: function(origin, destination, direction){},
-// 	afterLoad: function(origin, destination, direction){},
-// 	afterRender: function(){},
-// 	afterResize: function(width, height){},
-// 	afterReBuild: function(){},
-// 	afterResponsive: function(isResponsive){},
-// 	afterSlideLoad: function(section, origin, destination, direction){},
-// 	onSlideLeave: function(section, origin, destination, direction){}
-
-// });
-
-// //methods
-// fullpage_api.setAllowScrolling(false);
 
   // import Swiper JS
   import Swiper, { Navigation, Pagination } from 'swiper';
@@ -86,7 +8,7 @@
 
 // configure Swiper to use modules
 Swiper.use([Navigation, Pagination]);
-
+// маска тел
 window.addEventListener("DOMContentLoaded", function() {
     [].forEach.call( document.querySelectorAll('.tel'), function(input) {
     var keyCode;
@@ -124,7 +46,7 @@ window.addEventListener("DOMContentLoaded", function() {
     
     });
     
-
+// адаптив
     function fontSize() {
         if ($(window).width() < 900) {
           $('html').css({
@@ -172,7 +94,7 @@ window.addEventListener("DOMContentLoaded", function() {
     //   });
 
 
-
+// свайпер на главной
     let totale_slides = document.querySelector('.totale_slides')
     if (totale_slides) {
         let count_slides = document.querySelectorAll('.swiper_main .swiper-slide')
@@ -214,7 +136,10 @@ window.addEventListener("DOMContentLoaded", function() {
     //     }
     // }
 
+    // паралакс картинки
+
     const cards = document.querySelector(".screen1_img");
+    const cards7 = document.querySelectorAll(".screen7_img");
 // const images = document.querySelectorAll(".card__img");
 // const backgrounds = document.querySelectorAll(".card__bg");
 const range = 35;
@@ -233,6 +158,13 @@ document.addEventListener('mousemove', ({x, y}) => {
     const xValue = calcValue(x, window.innerWidth);
 
     cards.style.transform = `rotateX(${yValue}deg) rotateY(${xValue}deg)`;
+    cards7.forEach( (card) => {
+   
+      card.style.transform = `rotateX(${yValue}deg) rotateY(${xValue}deg)`;
+    })
+
+    
+    // cards2.style.transform = `rotateX(${yValue}deg) rotateY(${xValue}deg)`;
 
     // [].forEach.call(images, (image) => {
     //   image.style.transform = `translateX(${-xValue}px) translateY(${yValue}px)`;
@@ -243,6 +175,11 @@ document.addEventListener('mousemove', ({x, y}) => {
     // })
 	})
 }, false);
+
+
+
+
+// табы
 
 let tab_controls = document.querySelectorAll('#section2 .tab_control_item')
 let tab_contents = document.querySelectorAll('#section2 .tabs_content')
@@ -260,11 +197,27 @@ if (tab_controls.length > 0) {
      tab_contents[index].classList.add('show')
 
       // console.log(active_content, tab)
-    })
-
-  }
-  )
+    })}
+  )}
 
 
 
-}
+
+let tab_controls2 = document.querySelectorAll('#section7 .tab_control_item')
+let tab_contents2 = document.querySelectorAll('#section7 .tabs_content')
+
+if (tab_controls2.length > 0) {
+
+  tab_controls2.forEach((tab, index) => {
+   
+
+    tab.addEventListener('click', function(){
+
+
+     let active_content = document.querySelector('#section7 .tabs_content.show')
+     active_content.classList.remove('show')
+     tab_contents2[index].classList.add('show')
+
+      // console.log(active_content, tab)
+    })}
+  )}
